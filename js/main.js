af98e0ea7121ef5cc724d30836eb1297956db251
u10210018 <u10210018@go.utaipei.edu.tw>
2020-11-20 23:46:47 +0800

    first check

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let doFirst = function () {
+    let time = dayjs();
+    let timeChange = function () {
+        timeString = time.format('[GMT]Z YYYY-MM-DD HH:mm:ss');
+        document.getElementById('time_show').innerText = timeString;
+    };
+    timeChange();
+
+    let timeCount = setInterval(() => {
+        time = new dayjs();
+        timeChange();
+    }, 1000);
+
+}
+let openList = function (e) {
+    document.getElementById('choose_lang').classList.add('list-open');
+}
+
+let changeLang = function (e) {
+    e.stopPropagation();
+    document.getElementById('choose_lang').classList.remove('list-open');
+    document.getElementById('lang').innerText = e.currentTarget.innerText;
+};
+
+document.addEventListener('DOMContentLoad', doFirst());
+document.getElementById('choose_lang').addEventListener('click', openList);
+document.getElementById('lang1').addEventListener('click', changeLang);
+document.getElementById('lang2').addEventListener('click', changeLang);
+document.getElementById('lang3').addEventListener('click', changeLang);
